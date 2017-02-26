9ceaa80211d4f87a4e34d505f958eff7343d63fa
Brooke Werner <brookewerner@users.noreply.github.com>
2017-02-26 11:57:13 -0500

    updated color scheme, added instructions

--- eyemessage.js
+++ eyemessage.js
@@ -1,8 +1,9 @@
 var ctracker;
-var responses = ["yes", "no", "maybe", "hey!", "see you soon", "come over", "where are you?", "call me"];
+var responses = ["Hey!", "Yes", "No", "Maybe", "I am thirsty", "I am hungry", "How are you?"];
 var index = 0;
 var changed = false;
 var mouthOpen = false;
+var instructions = true;
 
 function setup() {
 
@@ -12,7 +13,7 @@ function setup() {
   videoInput.position(0, 0);
   
   //hide video feed
-  videoInput.hide();
+  //videoInput.hide();
   
   // setup canvas
   var cnv = createCanvas(800, 600);
@@ -27,21 +28,34 @@ function setup() {
 }
 
 function draw() {
-  background(255,255,255);
+  background(255);
+
   
-    // get array of face marker positions [x, y] format
+  stroke(100, 100, 100);
+  fill(200, 200, 200);
+  rect(10, 3*innerHeight/4+20, 330, 110 );
+  fill(0);
+  text("Welcome! The purpose of this application is to allow", 20, 3*innerHeight/4+50 );
+  text("you to communicate using only facial gestures.", 20, 3*innerHeight/4+65);
+  text("To change the words on the screen, move your eyebrows", 20, 3*innerHeight/4+80);
+  text("up and down. To select the phrase on the screen, open", 20, 3*innerHeight/4+95);
+  text("your mouth.", 20, 3*innerHeight/4+110);
+  
+  
+  // get array of face marker positions [x, y] format
   var positions = ctracker.getCurrentPosition();
   
   for (var i=0; i<positions.length; i++) {
     // set the color of the ellipse based on position on screen
-    fill(map(positions[i][0], width*0.33, width*0.66, 0, 255), map(positions[i][1], height*0.33, height*0.66, 0, 255), 255);
+    fill(20, 20, 60, 60);
+    noStroke();
     // draw ellipse at each position point
     ellipse(400-positions[i][0], positions[i][1], 10, 10);
   }
 
   if(positions.length > 0){
     fill(255, 0, 0);
-    ellipse(400-positions[62][0], positions[62][1], 10, 10);
+
     //top of eye and bottom of eyebrow coordinates
     var eyeX = positions[30][0];
     var eyeY = positions[30][1];
@@ -57,12 +71,11 @@ function draw() {
     var mouthDistance = getDistance(upMouthX, upMouthY, downMouthX, downMouthY);
     
     // console.log(browDistance);
-    words(browDistance, mouthDistance);
+    words(browDistance, mouthDistance, positions);
   }
-  
 }
 
-function words(browDistance, mouthDistance){
+function words(browDistance, mouthDistance, positions){
   textSize(75);
   stroke(255, 0, 0);
   fill(0);
@@ -70,6 +83,7 @@ function words(browDistance, mouthDistance){
   //eyebrows raised
   if(!mouthOpen) {
     if(browDistance>25){
+      instructions = false;
       if (!changed) {
         if(index <= responses.length) {
           index++;
@@ -83,24 +97,42 @@ function words(browDistance, mouthDistance){
       changed = false;
     }
   }
+  if (changed) {
+    for (var i=15; i<=22; i++) {
+      fill(255, 0, 0, 60);
+      noStroke();
+      ellipse(400-positions[i][0], positions[i][1], 10, 10);
+    }
+  }
   if (index >= responses.length) {
     index = 0;
   }
   console.log(index);
 
-  text(responses[index], innerWidth/2, innerHeight/2);
+  fill(0);
+  noStroke();
+  text(responses[index], 30, innerHeight/2+50);
   // console.log(index);
-  mouth(responses[index], mouthDistance);
+  mouth(responses[index], mouthDistance, positions);
 }
 
-function mouth(word, mouthDistance){
+function mouth(word, mouthDistance, positions){
 
   console.log(mouthDistance);
   
   if( mouthDistance > 18){
+    instructions = false;
     mouthOpen = true;
     fill(255, 0, 0);
-    text(word, innerWidth/2, innerHeight/2);
+    noStroke();
+    text(word, 30, innerHeight/2+50);
+
+    for (var i=44; i<=61; i++) {
+      noStroke();
+      fill(255, 0, 0, 60);
+      ellipse(400-positions[i][0], positions[i][1], 10, 10);
+    }
+
   } else {
     mouthOpen = false;
   }
